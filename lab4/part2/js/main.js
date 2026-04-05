4fe7eafc4f0219027f19e0f3421c01eab9af4c0c
Zacharyrr <zachary.thompson1015@gmail.com>
2026-04-04 21:22:33 -0400

    Update main.js
    create images constant as well as added downloaded images and created descrip/ alt texts for them

--- lab4/part2/js/main.js
+++ lab4/part2/js/main.js
@@ -3,3 +3,12 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+//creating images constant 
+const images = [
+    {filename: "pic1.jpg", alt: "Close up of a human eye"},
+    {filename: "pic2.jpg", alt: "Rock that looks like a w3ave"},
+    {filename: "pic3.jpg", alt: "Purple and white pansies"},
+    {filename: "pic4.jpg", alt: "section of a wall from a pharaoh's tomb"},
+    {filename: "pic5.jpg", alt: "Large moth on a leaf "},
+]
